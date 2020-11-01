a4c4e456350adcb7568dfc4ee46b743c50b6c479
ALbert2504 <albertarakelyan1998@gmail.com>
2020-11-02 00:56:57 +0400

    final commit

--- js/main.js
+++ js/main.js
@@ -28,14 +28,14 @@ function cardPorcess(cardElem) {
     const $cardEditBtn = cardElem.querySelector('.card__changeBtn');
     $cardEditBtn.addEventListener('click', function() {
         clickedPencil = this;
-        
+
         const cardPos = $cardEditBtn.parentElement.getBoundingClientRect();
-        
+
         let cardLeft = cardPos.left;
         let cardTop = cardPos.top;
         let cardWidth = cardPos.width;
         let cardHeight = cardPos.height;
-        
+
         const $cardNameChangeTxtArea = document.createElement('textarea');
         $cardNameChangeTxtArea.setAttribute('class', 'card__nameChange_txtArea');
         $cardNameChangeTxtArea.style = `
@@ -44,15 +44,16 @@ function cardPorcess(cardElem) {
             width: ${cardWidth}px;
             height: ${cardHeight + 45}px
         `;
-        
+
         this.remove();
 
+
         let cardPrevValue = cardElem.querySelector('.card__content').textContent;
-        
+
 
         const $bodyHalfOpacityBlackBg = document.createElement('div');
         $bodyHalfOpacityBlackBg.setAttribute('class', 'body__halfOpacity_blackBg');
-        
+
         document.body.insertAdjacentElement('afterbegin', $bodyHalfOpacityBlackBg);
         document.body.insertAdjacentElement('afterbegin', $cardNameChangeTxtArea);
         $cardNameChangeTxtArea.value = cardPrevValue;
@@ -60,7 +61,7 @@ function cardPorcess(cardElem) {
         $cardNameChangeTxtArea.select();
 
         $cardNameChangeTxtArea.addEventListener('blur', () => {
-            if(!$cardNameChangeTxtArea.value) {
+            if (!$cardNameChangeTxtArea.value) {
                 cardElem.querySelector('.card__content')
                     .innerText = cardPrevValue;
                 $cardNameChangeTxtArea.remove();
@@ -81,7 +82,6 @@ function cardPorcess(cardElem) {
 
 
 
-
 /******* cards drag and drop functions *******/
 let draggedCard = null;
 
@@ -89,71 +89,76 @@ function dragstart_Card(e) {
     draggedCard = this;
     this.classList.add('dragging');
 }
+
 function dragend_Card(e) {
     draggedCard = null;
     this.classList.remove('dragging');
 
     document.querySelectorAll('.card').forEach(c => c.classList.remove('under'));
 }
+
 function dragenter_Card(e) {
-    if(this === draggedCard) {
+    if (this === draggedCard) {
         return;
     }
 
-    if(draggedCard) {
+    if (draggedCard) {
         this.classList.add('under');
-        if(e.target.tagName === 'P') {
+        if (e.target.tagName === 'P') {
             this.classList.add('under');
         }
     }
 }
+
 function dragover_Card(e) {
     e.preventDefault();
-    if(this === draggedCard) {
+    if (this === draggedCard) {
         return;
     }
-    if(draggedCard) {
-        if(e.target.tagName === 'P') {
+    if (draggedCard) {
+        if (e.target.tagName === 'P') {
             this.classList.add('under');
         }
     }
 
     // console.log(e.target.tagName);
 }
+
 function dragleave_Card(e) {
-    if(this === draggedCard) {
+    if (this === draggedCard) {
         return;
     }
     this.classList.remove('under');
-    if(draggedCard) {
-        if(e.target.tagName === 'P') {
+    if (draggedCard) {
+        if (e.target.tagName === 'P') {
             this.classList.add('under');
         }
     }
 
 }
+
 function drop_Card(e) {
-    if(draggedCard) {
+    if (draggedCard) {
         e.stopPropagation();
     }
-    if(this === draggedCard) {
+    if (this === draggedCard) {
         return;
     }
 
-    if(!draggedCard) {
+    if (!draggedCard) {
         this.classList.remove('under');
         return;
     }
 
-    if(this.parentElement === draggedCard.parentElement) {
+    if (this.parentElement === draggedCard.parentElement) {
         const card = Array.from(this.parentElement.querySelectorAll('.card'));
         let indexA = card.indexOf(this);
         let indexB = card.indexOf(draggedCard);
-        if(indexA < indexB) {
+        if (indexA < indexB) {
             this.parentElement.insertBefore(draggedCard, this);
         } else {
             this.parentElement.insertBefore(draggedCard, this.nextElementSibling);
-        }   
+        }
     } else {
         this.parentElement.insertBefore(draggedCard, this);
     }
@@ -172,7 +177,7 @@ function columnProcces(columnElem) {
         const colHeadingChange = columnElem.querySelector('.column__heading_change');
         const colHeading = columnElem.querySelector('.column__heading');
         var colHeadingPrevValue = colHeading.innerText;
-        
+
         colHeading.style.display = 'none';
         colHeadingChange.style.display = 'block';
         colHeadingChange.value = colHeadingPrevValue;
@@ -180,7 +185,7 @@ function columnProcces(columnElem) {
         colHeadingChange.select();
 
         colHeadingChange.addEventListener('blur', () => {
-            if(!colHeadingChange.value) {
+            if (!colHeadingChange.value) {
                 colHeadingChange.style.display = 'none';
                 colHeading.style.display = 'block';
                 colHeading.innerHTML = colHeadingPrevValue;
@@ -191,7 +196,7 @@ function columnProcces(columnElem) {
             }
         })
     });
-    
+
 
     //adding notes
     columnElem.querySelector('[data-action-addNote]').addEventListener('click', e => {
@@ -200,7 +205,7 @@ function columnProcces(columnElem) {
         card.setAttribute('data-card-id', cardCount);
         card.setAttribute('draggable', 'true');
         cardCount++;
-        
+
         const cardName = document.createElement('textarea');
         cardName.setAttribute('class', 'card__name');
 
@@ -230,7 +235,7 @@ function columnProcces(columnElem) {
 
 
         cardAccept.addEventListener('click', () => {
-            if(!cardName.value) {
+            if (!cardName.value) {
                 cardBox.removeChild(cardAccept);
                 cardBox.removeChild(cardCancel);
                 cardBox.removeChild(cardName);
@@ -253,7 +258,7 @@ function columnProcces(columnElem) {
                 columnElem.querySelector('[data-action-addnote]').style.display = 'block';
                 cardPorcess(card);
             }
-        });  
+        });
 
 
     });
@@ -267,38 +272,58 @@ function columnProcces(columnElem) {
         draggedColumn = null;
     });
     columnElem.addEventListener('dragenter', function(e) {
-        if(this === draggedColumn) {
+        if (this === draggedColumn) {
             return;
         }
     });
-    columnElem.addEventListener('dragover', function (e) {
+    columnElem.addEventListener('dragover', function(e) {
         e.preventDefault();
-        if(this === draggedColumn) {
+        if (this === draggedColumn) {
             return;
         }
     });
     columnElem.addEventListener('dragleave', function(e) {
-        if(this === draggedColumn) {
+        if (this === draggedColumn) {
             return;
         }
     });
-    columnElem.addEventListener('drop', function (e) {
+    columnElem.addEventListener('drop', function(e) {
         e.stopPropagation();
-        if(draggedCard) {
+        if (draggedCard) {
             columnElem.querySelector('[data-cards]').appendChild(draggedCard);
-        } else if(draggedColumn) {
+        } else if (draggedColumn) {
             const column = Array.from(this.parentElement.querySelectorAll('.column'));
             let indexA = column.indexOf(this);
             let indexB = column.indexOf(draggedColumn);
-            if(indexA < indexB) {
+            if (indexA < indexB) {
                 this.parentElement.insertBefore(draggedColumn, this);
             } else {
                 this.parentElement.insertBefore(draggedColumn, this.nextElementSibling);
-            }   
+            }
         }
     });
 
+
+    // column configs window filling and styling
     const columnConfigs = document.createElement('div');
+    columnConfigs.setAttribute('class', 'column__configs');
+    columnConfigs.innerHTML = `
+        <h5 class="configs__heading light">Actions with the list</h5>
+        <span class="configs__closeBtn awesomesolid">window-close</span>
+        <div class="configs__separator"></div>
+        <span class="configs__action light">Add card...</span>
+        <span class="configs__action light">Copy list...</span>
+        <span class="configs__action light">Move list...</span>
+        <span class="configs__action light">Subscribe</span>
+        <div class="configs__separator"></div>
+        <span class="configs__action light">Sort by...</span>
+        <div class="configs__separator"></div>
+        <span class="configs__action light">Move all the cards...</span>
+        <span class="configs__action light">Archive all the cards...</span>
+        <span class="configs__action light">Archive the list...</span>
+    `;
+
+
     const columnConfigBtn = columnElem.querySelector('.column__menu');
     columnConfigBtn.addEventListener('click', function() {
 
@@ -306,17 +331,20 @@ function columnProcces(columnElem) {
         let configBtnLeft = ConfigBtnPos.left;
         let configBtnTop = ConfigBtnPos.top;
 
+        let currentColumn = this.parentElement.parentElement;
+
+        document.body.insertAdjacentElement('afterbegin', columnConfigs);
+        const columnConfigsWidth = columnConfigs.getBoundingClientRect();
+        const ColumnConfigCloseBtn = columnConfigs.querySelector('.configs__closeBtn');
+        ColumnConfigCloseBtn.addEventListener('click', function() {
+            this.parentElement.remove();
+        });
+
+
         columnConfigs.style = `
             left: ${configBtnLeft}px;
+            top: ${configBtnTop + 23}px;
         `;
-
-        var currentColumn = this.parentElement.parentElement;
-        
-        columnConfigs.setAttribute('class', 'column__configs');
-
-        currentColumn.insertAdjacentElement('afterbegin', columnConfigs);
-
-        
 
     });
 }
@@ -340,12 +368,12 @@ $addColumnBtn.addEventListener('click', e => {
     inp.setAttribute('class', 'columnName');
     columnOptions.appendChild(inp);
     inp.focus();
-    
+
 
     const btn = document.createElement('button');
     btn.setAttribute('class', 'acceptBtn')
     btn.innerHTML = 'Add list';
-    
+
     const cancelBtn = document.createElement('button');
     cancelBtn.setAttribute('class', 'cancelBtn');
     cancelBtn.innerHTML = 'Cancel';
@@ -363,7 +391,7 @@ $addColumnBtn.addEventListener('click', e => {
 
     btn.addEventListener('click', e => {
         e.preventDefault();
-        if(!inp.value) {
+        if (!inp.value) {
             alert('Please choose a name for the board.');
         } else {
             const column = document.createElement('div');
@@ -398,7 +426,7 @@ $addColumnBtn.addEventListener('click', e => {
 
             $subColumns.appendChild(column);
             column.appendChild(colHeadingMenu);
-            
+
             colHeadingMenu.appendChild(ColHeadingChange);
             colHeadingMenu.appendChild(colHeading);
             colHeadingMenu.appendChild(colMenu);
@@ -408,7 +436,7 @@ $addColumnBtn.addEventListener('click', e => {
 
             $subColumns.removeChild(columnOptions);
             $addColumnBtn.style.display = 'block';
-            
+
             columnProcces(column);
 
         }
